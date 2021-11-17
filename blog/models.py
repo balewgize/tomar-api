eb9097afa0ffa4539b846ebd9e942f92e45e4d3e
Alemnew Marie <alemnewmarie461@gmail.com>
2021-11-17 23:45:09 +0300

    Build blog model, add utility functions

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,71 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+from .utils import get_unique_slug, get_post_image_path, get_compressed_image
+
+
+class Category(models.Model):
+    """Categories used to organize posts."""
+
+    title = models.CharField(max_length=40)
+    slug = models.SlugField(unique=True)  # auto generate
+
+    class Meta:
+        verbose_name = "category"
+        verbose_name_plural = "categories"
+
+    def __str__(self) -> str:
+        return self.title
+
+    def save(self, *args, **kwargs):
+        # assing a unique slug from the title of a category
+        self.slug = get_unique_slug(self.__class__, self.title)
+        return super().save(*args, **kwargs)
+
+
+class Post(models.Model):
+    """Posts written by admin or users of the platform."""
+
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    image = models.ImageField(
+        get_post_image_path,
+        help_text="Select an appropriate image for your post. Good images increase the chance of being read.",
+    )
+    slug = models.SlugField(max_length=212, unique=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="posts"
+    )
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    date_posted = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-date_posted", "title"]
+
+    def __str__(self) -> str:
+        return self.title
+
+    def save(self, *args, **kwargs):
+        self.image = get_compressed_image(self.image)
+        self.slug = get_unique_slug(self.__class__, self.title)
+        return super().save(*args, **kwargs)
+
+
+class Comment(models.Model):
+    """User's comment on posts."""
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    content = models.TextField(verbose_name="")
+    date_posted = models.DateTimeField(auto_now=True)
+
+
+class Reply(models.Model):
+    """Replies to a comment."""
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    comment = models.ForeignKey(
+        Comment, on_delete=models.CASCADE, related_name="replies"
+    )
+    content = models.TextField(verbose_name="")
+    date_posted = models.DateTimeField(auto_now=True)
