30a9e4d05e289e213af0da7b23e913157ae55088
Alemnew Marie <alemnewmarie461@gmail.com>
2021-11-18 08:49:41 +0300

    Build accounts model with custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,28 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from utils import utils
+
+
+class User(AbstractUser):
+    """Custom user model with unique email."""
+
+    email = models.EmailField(unique=True)
+
+
+class Profile(models.Model):
+    """User's profile."""
+
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, primary_key=True
+    )
+    title = models.CharField(max_length=50)
+    bio = models.TextField()
+    avatar = models.ImageField(
+        default="default-avatar.jpg", upload_to=utils.get_avatar_image_path
+    )
+
+    def save(self, *args, **kwargs):
+        self.avatar = utils.get_compressed_image(self.avatar, size=(300, 300))
+        return super().save(*args, **kwargs)
